cc920d17f9f5cb526255cbf3145ce7a9a7c9c6bd
Achintya0502 <achintyapatel55555@gmail.com>
2020-08-25 22:15:46 +0530

    user registration

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const connectDB = require('./config/db');
 const app = express();
 connectDB();
 
+app.use(express.json({ extended: false }));
+
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
