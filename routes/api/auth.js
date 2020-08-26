d9e6a22ddc59a1da6b87fa03dd7ee665524cc53d
Achintya0502 <achintyapatel55555@gmail.com>
2020-08-26 15:30:32 +0530

    jwt verify

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../../models/User');
+const auth = require('../../middleware/auth');
 
-router.get('/', (req, res) => {
-  res.send('Route');
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
 });
 
 module.exports = router;
